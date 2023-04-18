d036ea639899d390c69aa3ddec0035078133c3b5
Cesar Henrique Martins da Rosa <chmr.developer@gmail.com>
2023-04-17 22:33:00 -0300

    Repository created

---
+++ index.ts
@@ -0,0 +1,37 @@
+export default class CreateHTMLElement{
+    _element: HTMLElement
+
+    constructor(elementType: string){
+        this._element = document.createElement(elementType)
+        return this
+    }
+
+    public SetId(id: string){
+        this._element.id = id
+        return this
+    }
+
+    public AddClass(className: string){
+        this._element.classList.add(className)
+        return this
+    }
+
+    public RemoveClass(className: string){
+        this._element.classList.remove(className)
+        return this
+    }
+
+    public SetClass(className: string){
+        this._element.className = className
+        return this
+    }
+
+    public AppendTo(parent: HTMLElement){
+        parent.appendChild(this._element)
+        return this
+    }
+
+    public GetElementAs<T>(){
+        return this._element as T
+    }
+}
