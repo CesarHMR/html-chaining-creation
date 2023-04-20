3dd512b0a217cba2d850d2512b4e460ebb76306c
Cesar Henrique Martins da Rosa <chmr.developer@gmail.com>
2023-04-20 00:11:13 -0300

    Using camelCase for functions name

--- src/CreateHTMLElement.ts
+++ src/CreateHTMLElement.ts
@@ -8,37 +8,37 @@ export class CreateHTMLElement{
         return this
     }
 
-    public SetId(id: string){
+    public setId(id: string){
         this._element.id = id
         return this
     }
 
-    public AddClass(className: string){
+    public addClass(className: string){
         this._element.classList.add(className)
         return this
     }
 
-    public SetClass(className: string){
+    public setClass(className: string){
         this._element.className = className
         return this
     }
 
-    public AppendTo(parent: HTMLElement){
+    public appendTo(parent: HTMLElement){
         parent.appendChild(this._element)
         return this
     }
 
-    public AddEventListener(event: string, callback: Function){
+    public addEventListener(event: string, callback: Function){
         this._element.addEventListener(event, (event) => callback)
         return this
     }
 
-    public SetInnerHTML(innerHTML: string){
+    public setInnerHTML(innerHTML: string){
         this._element.innerHTML = innerHTML
         return this
     }
 
-    public SetInnerText(innerText: string){
+    public setInnerText(innerText: string){
         this._element.innerText = innerText
         return this
     }
@@ -55,70 +55,70 @@ export class CreateHTMLElement{
         return this
     }
 
-    public SetName(name: string){
+    public setName(name: string){
         if('name' in this._element) {
             this._element.name = name
         }
         return this
     }
 
-    public SetTarget(target: string){
+    public setTarget(target: string){
         if('target' in this._element) {
             this._element.target = target
         }
         return this
     }
 
-    public SetValue(value: string){
+    public setValue(value: string){
         if('value' in this._element) {
             this._element.value = value
         }
         return this
     }
 
-    public SetType(type: string){
+    public setType(type: string){
         if('type' in this._element){
             (this._element as HTMLInputElement).type = type
         }
         return this
     }
 
-    public SetMaxValue(max: string){
+    public setMaxValue(max: string){
         if('max' in this._element){
             this._element.max = max
         }
         return this
     }
 
-    public SetMinValue(min: string){
+    public setMinValue(min: string){
         if('min' in this._element){
             this._element.min = min
         }
         return this
     }
 
-    public SetMaxLenght(maxLength: number){
+    public setMaxLenght(maxLength: number){
         if('maxLength' in this._element){
             this._element.maxLength = maxLength
         }
         return this
     }
 
-    public SetMinLenght(minLength: number){
+    public setMinLenght(minLength: number){
         if('minLength' in this._element){
             this._element.minLength = minLength
         }
         return this
     }
 
-    public SetPlaceholder(placeholder: string){
+    public setPlaceholder(placeholder: string){
         if('placeholder' in this._element){
             this._element.placeholder = placeholder
         }
         return this
     }
 
-    public GetElement(){
+    public getElement(){
         return this._element
     }
 }
